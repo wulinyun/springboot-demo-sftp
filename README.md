c24dafcb1fa522fa43b7415e6df5d58aa8662e88
JasonTang <212868633@qq.com>
2019-03-19 15:43:49 +0800

    update readme file

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 使用jsch Jar实现SFTP上传、下载、创建、删除文件
 
 #### 使用說明
-具体使用说明：[这里](http://www.baidu.com)
+具体使用说明：[这里](https://www.jianshu.com/p/776df6809c5e)
--- src/test/java/com/gotrade/sftp/SftpApplicationTests.java
+++ src/test/java/com/gotrade/sftp/SftpApplicationTests.java
@@ -14,12 +14,7 @@ import java.io.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SftpApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-
+    
     @Autowired
     private FileSystemService fileSystemService;
 
